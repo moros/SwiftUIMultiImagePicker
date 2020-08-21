7b3cd50f3cc2f22895ea9c38e53242203c08195b
Doug Mason <dmason@unitedfiregroup.com>
2020-08-21 09:52:45 -0500

    Fixed issue with dropdown content size.

--- SwiftUIImagePicker/MultiImagePicker/AlbumsViewModel.swift
+++ SwiftUIImagePicker/MultiImagePicker/AlbumsViewModel.swift
@@ -6,12 +6,21 @@
 //  Copyright © 2020 United Fire Group. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 public class AlbumsViewModel: NSObject {
     
     var navigationController: UINavigationController? = nil
     private var transitionDelegate: DropdownTransitionDelegate? = nil
+    
+    var isLandscape: Bool {
+        return UIApplication.shared.windows
+            .first?
+            .windowScene?
+            .interfaceOrientation
+            .isLandscape ?? false
+    }
     
     @objc func albumsButtonPressed(_ sender: UIButton) {
 
@@ -21,13 +30,18 @@ public class AlbumsViewModel: NSObject {
         }
         
         let delegate = DropdownTransitionDelegate()
+        
+        let window: UIWindow? = UIApplication.shared.windows.first
         delegate.sourceView = sender
-        delegate.sourceRect = sender.convert(sender.bounds, to: UIApplication.shared.windows.first)
+        delegate.sourceRect = sender.convert(sender.bounds, to: window)
         self.transitionDelegate = delegate
         
         controller.modalPresentationStyle = .custom
         controller.transitioningDelegate = self.transitionDelegate
-        controller.preferredContentSize = CGSize(width: 200, height: 400)
+        
+        let frame = window?.frame ?? CGRect.zero
+        let height = frame.height * (self.isLandscape ? 0.75 : 0.5)
+        controller.preferredContentSize = CGSize(width: frame.width * 0.75, height: height)
         rotateButtonArrow(sender)
         
         self.navigationController?.present(controller, animated: true, completion: nil)
